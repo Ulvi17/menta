98f9286accd0022ae319d51a8412756012a2b407
Ulvi17 <aaliyevulvi@gmail.com>
2025-07-29 21:54:04 +0400

    Update bagira-ai.ts

--- config/bagira-ai.ts
+++ config/bagira-ai.ts
@@ -3,6 +3,6 @@ export const bagiraAIConfig = {
   supabaseUrl: "https://wirwojaiknnvtpzaxzjv.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpcndvamFpa25udnRwemF4emp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NjE3ODcsImV4cCI6MjA2NjUzNzc4N30.XyhklppW2bvJQ7qFv4SWaDaGK_M_YoGFAiOIFo2tW1c",
   channel: "bagheera:new-call",
-  assistantId: "541e48cf-1f4a-423b-875f-889c6917a56d",
+  assistantId: "499834bd-fa62-4a23-9de0-798966ff895e",
   webhookUrl: "https://primary-w2wb-edtechstudio.up.railway.app/webhook/toolCall"
 }; 
